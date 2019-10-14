0779050c1c593e3ca11dfdec0442ce73aafd4cb8
MartimViana <martimviana98@gmail.com>
2019-10-14 14:29:47 +0100

    Add basic physic formulas on interface.py

--- src/interface.py
+++ src/interface.py
@@ -23,8 +23,23 @@ def wattsToNewtons(watts, seconds, distance):
 def newtonsToWatts(newtons, seconds, distance):
 	return joulesToWatts(newtons / distance, seconds)
 
+def speed(initialDistance, finalDistance, initialTime, finalTime):
+	return (finalDistance - initialDistance) / (finalTime - initialTime)
+
+def acceleration(initialSpeed, finalSpeed, initialTime, finalTime):
+	return speed(initialSpeed, finalSpeed, initialTime, finalTime)
+
+def force(mass, acceleration):
+	return mass * acceleration
+
+
+def distance(initialX, finalX, initialY, finalY):
+	return math.sqrt((finalX - initialX)*(finalX - initialX) + (finalY - initialY)*(finalY - initialY))
+
+def distance(initialVelocity, time, acceleration):
+	return initialVelocity*time + (1/2)*acceleration*time*time
+
 ## Ballistical-related calculus
 def calculateBulletSpeed(pistonPressure, volumicMassAir, exitRadius, pistonRadius):
 	return math.sqrt((pistonPressure - atmosphericPressure)/(volumicMassAir*(1-((exitRadius*exitRadius)/(pistonRadius*pistonRadius)))))
 
-	
